6fd726fa7b28f8df5720ab7d6cadaae2011e0849
Andrey Gladyshev <vodomerkin@gmail.com>
2018-08-11 17:27:55 +0600

    Remove animations added.

--- Item.gd
+++ Item.gd
@@ -1,8 +1,19 @@
 extends Node2D
 
+const DISSAPEAR_ANIMATIONS = [
+	"Dissapear1",
+	"Dissapear2"
+]
+
 var itemType = null
 
 func setType(typeName):
 	itemType = typeName
-	for child in get_children():
+	for child in $Graphic.get_children():
 		child.visible = child.name == typeName
+
+func scheduleForDeletion():
+	queue_free()
+
+func startRemoving():
+	$AnimationPlayer.play(DISSAPEAR_ANIMATIONS[randi() % DISSAPEAR_ANIMATIONS.size()])
