a47e739e16c7c8ea9491f459879ff335458621a8
Andrey Gladyshev <vodomerkin@gmail.com>
2018-08-13 02:56:11 +0600

    Choosing hero on title screen.

---
+++ Title.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var global = null
+var sidescroller = null
+
+func _ready():
+	global = get_node("/root/Global")
+	assert global != null
+	
+	sidescroller = get_node("../Sidescroller")
+	assert sidescroller != null
+
+
+func startWithSpeed(speed):
+	global.speed = speed
+	$AnimationPlayer.play("Dissolve")
+	sidescroller.moving = true
+
+func scheduleForRemoval():
+	queue_free()
+
+func _on_Knight_pressed():
+	startWithSpeed(1.0)
+
+
+func _on_Sorceress_pressed():
+	startWithSpeed(1.8)
+
+func _on_Ninja_pressed():
+	startWithSpeed(3.0)
